c9b4eb4a7f0e1fb6b7e3bbde63a052872da0d42e
morooka <morooka@logiclogic.jp>
2020-03-15 14:14:36 +0900

    売買履歴一旦

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -3,6 +3,7 @@ module Api::V1
     before_action :authenticate_api_v1_user!
 
     def index
+      render json: {status: 'SUCCESS', data: {buy: current_api_v1_user.buy_histories, sell: current_api_v1_user.sell_histories}}
     end
   end
 end
--- spec/controllers/api/v1/histories_controller_spec.rb
+++ spec/controllers/api/v1/histories_controller_spec.rb
@@ -1,6 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::HistoriesController, type: :request do
+  before do
+    @owner_user = FactoryBot.create(:base_user)
+    @other_user = FactoryBot.create(:base_user2)
+    post "/api/v1/auth/sign_in", FactoryBot.attributes_for(:base_user)
+    @headers = {
+      'Content-Type' => 'application/json',
+      'access-token' => response.header["access-token"],
+      'client' => response.header["client"],
+      'uid' => response.header["uid"]
+    }
+
+    # ユーザーの商品を作成する
+    @owner_items = FactoryBot.build_list(:items, 5)
+    @owner_items.each{|item| item.exhibit_flag = true; item.create_user_id = @owner_user.id; item.save}
+    @other_items = FactoryBot.build_list(:items, 5)
+    @other_items.each{|item| item.exhibit_flag = true; item.create_user_id = @other_user.id; item.save}
+
+    # 売買履歴を作成する
+    @owner_items.each do |item|
+      Item.buy(item, @other_user)
+    end
+    @other_items.each do |item|
+      Item.buy(item, @owner_user)
+    end
+
+    @owner_user.reload
+    @buy_histories = @owner_user.buy_histories.map{|history| history.attributes }
+    @buy_histories.each{|history| history["created_at"] = history["created_at"].strftime("%Y-%m-%dT%H:%M:%S.%3NZ"); history["updated_at"] = history["updated_at"].strftime("%Y-%m-%dT%H:%M:%S.%3NZ")}
+    @sell_histories = @owner_user.sell_histories.map{|history| history.attributes }
+    @sell_histories.each{|history| history["created_at"] = history["created_at"].strftime("%Y-%m-%dT%H:%M:%S.%3NZ"); history["updated_at"] = history["updated_at"].strftime("%Y-%m-%dT%H:%M:%S.%3NZ")}
+  end
+
   it "show histories" do
+    get "/api/v1/histories", headers: @headers
+    expect(response.code).to eq("200")
+    body = JSON.parse(response.body)
+    expect(body["data"]["buy"]).to match_array(@buy_histories)
+    expect(body["data"]["sell"]).to match_array(@sell_histories)
+  end
+
+  it "show unautholized histories" do
+    get "/api/v1/histories"
+    expect(response.code).to eq("401")
   end
 end
